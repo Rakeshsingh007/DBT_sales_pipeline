75ff94cfdcf7aceb584e0e715029177b3931d6bd
Rakesh Singh <rakesh.singh@kyruus.com>
2024-08-28 01:02:11 +0530

    27 Aug Changes

--- models/liquor_sales_analysis.sql
+++ models/liquor_sales_analysis.sql
@@ -1,44 +1,36 @@
-{{ config(materialized="table", alias="sales_analysis") }}
-
 with
-
-    -- Calculate total sales
-    total_sales as (
-        select sum(amount) as total_sales from {{ ref("stg_liquor_sales") }}
+    total_sales  /* Calculate total sales */
+    as (
+        select sum(amount) as total_sales
+        from {{ ref("stg_liquor_sales") }} as stg_liquor_sales
     ),
-
-    -- Calculate sales by category
-    sales_by_category as (
+    sales_by_category  /* Calculate sales by category */
+    as (
         select category_name, sum(amount) as category_sales
-        from {{ ref("stg_liquor_sales") }}
+        from "sales_analytics"."public"."stg_liquor_sales" as stg_liquor_sales
         group by category_name
     ),
-
-    -- Calculate sales by store location
-    sales_by_store as (
+    sales_by_store  /* Calculate sales by store location */
+    as (
         select store_location, sum(amount) as store_sales
-        from {{ ref("stg_liquor_sales") }}
+        from {{ ref("stg_liquor_sales") }} as stg_liquor_sales
         group by store_location
     ),
-
-    -- Calculate sales by item
-    top_selling_items as (
+    top_selling_items  /* Calculate sales by item */
+    as (
         select item_name, sum(quantity) as quantity_sold
-        from {{ ref("stg_liquor_sales") }}
+        from "sales_analytics"."public"."stg_liquor_sales"
         group by item_name
-        order by quantity_sold desc
+        order by quantity_sold desc nulls last
         limit 10
     ),
-
-    -- Calculate sales by month
-    sales_by_month as (
+    sales_by_month  /* Calculate sales by month */
+    as (
         select
-            date_trunc('month', order_date) as sales_month, sum(amount) as monthly_sales
-        from {{ ref("stg_liquor_sales") }}
+            date_trunc('MONTH', order_date) as sales_month, sum(amount) as monthly_sales
+        from "sales_analytics"."public"."stg_liquor_sales"
         group by sales_month
     )
-
--- Combine all analysis into a single table
 select
     total_sales.total_sales,
     sales_by_category.category_name,
